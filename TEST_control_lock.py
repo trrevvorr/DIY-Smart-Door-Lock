8311db702b940e0203293f570315dbc2f50fe116
Trevor Ross <trevor.is.ross@gmail.com>
2018-08-12 23:31:33 +0000

    added rest of unit tests

--- TEST_control_lock.py
+++ TEST_control_lock.py
@@ -2,24 +2,54 @@ import unittest
 import RPi.GPIO as GPIO
 import mock
 import _control_lock
+import time
 
 import commands
 import pins
 import settings
 
 
-class TestStringMethods(unittest.TestCase):
+class TestServoControls(unittest.TestCase):
 
 	"""
 	SETUP AND TEARDOWN
 	"""
 	def setUp(self):
-		self.sleep_patch = mock.patch("time.sleep", autospec=True)
-		self.sleep_patch.start()
+		self._sleep_patch = mock.patch("time.sleep", autospec=True)
+		self.sleep_mock = self._sleep_patch.start()
+		self._output_patch = mock.patch("RPi.GPIO.output", autospec=True)
+		self.output_mock = self._output_patch.start()
+		self._PWM_patch = mock.patch("RPi.GPIO.PWM", autospec=True)
+		self.PWM_mock = self._PWM_patch.start()
+		self._setup_patch = mock.patch("RPi.GPIO.setup", autospec=True)
+		self.setup_mock = self._setup_patch.start()
+		self._setwarnings_patch = mock.patch("RPi.GPIO.setwarnings", autospec=True)
+		self.setwarnings_mock = self._setwarnings_patch.start()
+		self._setmode_patch = mock.patch("RPi.GPIO.setmode", autospec=True)
+		self.setmode_mock = self._setmode_patch.start()
 
 	def tearDown(self):
-		self.sleep_patch.stop()
+		self._sleep_patch.stop()
+		self._output_patch.stop()
+		self._PWM_patch.stop()
+		self._setup_patch.stop()
+		self._setwarnings_patch.stop()
+		self._setmode_patch.stop()
 
+	"""
+	TEST SETUP MOCKS
+	"""
+	def test_setup_mocks(self):
+		assert self.sleep_mock is time.sleep
+		assert self.output_mock is GPIO.output
+		assert self.PWM_mock is GPIO.PWM
+		assert self.setup_mock is GPIO.setup
+		assert self.setwarnings_mock is GPIO.setwarnings
+		assert self.setmode_mock is GPIO.setmode
+
+	"""
+	TEST INVALID commmands
+	"""
 	def test_invalid_command_fails(self):
 		with self.assertRaises(NotImplementedError):
 			_control_lock.main("BOGUS")
@@ -27,77 +57,92 @@ class TestStringMethods(unittest.TestCase):
 	"""
 	TESTING _setup
 	"""
-	@mock.patch("RPi.GPIO.setwarnings", autospec=True)
 	@mock.patch("_control_lock._lock", autospec=True)
-	def test_setup_setwarnings(self, mock__lock, mock_setwarnings):
+	def test_setup_setwarnings(self, mock__lock):
 		assert mock__lock is _control_lock._lock
-		assert mock_setwarnings is GPIO.setwarnings
+		assert self.setwarnings_mock is GPIO.setwarnings
 		_control_lock.main(commands.LOCK)
-		mock_setwarnings.assert_called_once_with(False)
+		self.setwarnings_mock.assert_called_once_with(False)
 
 
-	@mock.patch("RPi.GPIO.setmode", autospec=True)
 	@mock.patch("_control_lock._lock", autospec=True)
-	def test_setup_setmode(self, mock__lock, mock_setmode):
+	def test_setup_setmode(self, mock__lock):
 		assert mock__lock is _control_lock._lock
-		assert mock_setmode is GPIO.setmode
 		_control_lock.main(commands.LOCK)
-		mock_setmode.assert_called_once_with(GPIO.BOARD)
+		self.setmode_mock.assert_called_once_with(GPIO.BOARD)
 
-	@mock.patch("RPi.GPIO.setup", autospec=True)
 	@mock.patch("_control_lock._lock", autospec=True)
-	def test_setup_setup(self, mock__lock, mock_setup):
+	def test_setup_setup(self, mock__lock):
 		assert mock__lock is _control_lock._lock
-		assert mock_setup is GPIO.setup
 		_control_lock.main(commands.LOCK)
 		calls = [mock.call(pins.SERVO_PIN, GPIO.OUT),
 				 mock.call(pins.LOCK_STATUS_LED_PIN, GPIO.OUT),
 				 mock.call(pins.BUZZER_PIN, GPIO.OUT)]
-		mock_setup.assert_has_calls(calls, any_order=True)
+		self.setup_mock.assert_has_calls(calls, any_order=True)
 
-	@mock.patch("RPi.GPIO.PWM", autospec=True)
 	@mock.patch("_control_lock._lock", autospec=True)
-	def test_setup_PMW(self, mock__lock, mock_PWM):
+	def test_setup_PMW(self, mock__lock):
 		assert mock__lock is _control_lock._lock
-		assert mock_PWM is GPIO.PWM
 		_control_lock.main(commands.LOCK)
-		mock_PWM.assert_called_once_with(pins.SERVO_PIN, 50)
+		self.PWM_mock.assert_called_once_with(pins.SERVO_PIN, 50)
 
-	@mock.patch("RPi.GPIO.output", autospec=True)
 	@mock.patch("_control_lock._lock", autospec=True)
-	def test_setup_output(self, mock__lock, mock_output):
+	def test_setup_output(self, mock__lock):
 		assert mock__lock is _control_lock._lock
-		assert mock_output is GPIO.output
 		_control_lock.main(commands.LOCK)
-		mock_output.assert_called_once_with(pins.BUZZER_PIN,GPIO.LOW)
+		self.output_mock.assert_called_once_with(pins.BUZZER_PIN,GPIO.LOW)
 
 	"""
 	TESTING _lock
 	"""
-	@mock.patch("RPi.GPIO.output", autospec=True)
 	@mock.patch("_control_lock._setup", autospec=True)
-	def test_lock_output(self, mock__setup, mock_output):
+	def test_lock_output(self, mock__setup):
 		mock__setup.return_value = GPIO.PWM(pins.SERVO_PIN, 50)
 		assert mock__setup is _control_lock._setup
-		assert mock_output is GPIO.output
 		_control_lock.main(commands.LOCK)
-		mock_output.assert_called_once_with(pins.LOCK_STATUS_LED_PIN, GPIO.HIGH)
+		self.output_mock.assert_called_once_with(pins.LOCK_STATUS_LED_PIN, GPIO.HIGH)
 
-	@mock.patch("RPi.GPIO.output", autospec=True)
 	@mock.patch("_control_lock._setup", autospec=True)
 	@mock.patch("__main__.MockServo", autospec=True)
-	# @mock.patch("__main__.MockServo.stop", autospec=True)
-	def test_lock_start_stop(self, mock_MockServo, mock__setup, mock_output):
+	def test_lock_start_stop(self, mock_MockServo, mock__setup):
 		mock__setup.return_value = MockServo()
 		assert mock__setup is _control_lock._setup
-		assert mock_output is GPIO.output
 		assert mock_MockServo is MockServo
-		# assert mock_stop is MockServo.stop
 		_control_lock.main(commands.LOCK)
 		mock_MockServo.return_value.start.assert_called_once_with(settings.SERVO_LOCKED_POSITION)
 		mock_MockServo.return_value.stop.assert_called_once_with()
 
+	"""
+	TESTING _unlock
+	"""
+	@mock.patch("_control_lock._setup", autospec=True)
+	def test_unlock_output(self, mock__setup):
+		mock__setup.return_value = GPIO.PWM(pins.SERVO_PIN, 50)
+		assert mock__setup is _control_lock._setup
+		_control_lock.main(commands.UNLOCK)
+		self.output_mock.assert_called_once_with(pins.LOCK_STATUS_LED_PIN, GPIO.LOW)
 
+	@mock.patch("_control_lock._setup", autospec=True)
+	@mock.patch("__main__.MockServo", autospec=True)
+	def test_unlock_start_stop(self, mock_MockServo, mock__setup):
+		mock__setup.return_value = MockServo()
+		assert mock__setup is _control_lock._setup
+		assert mock_MockServo is MockServo
+		_control_lock.main(commands.UNLOCK)
+		mock_MockServo.return_value.start.assert_called_once_with(settings.SERVO_UNLOCKED_POSITION)
+		mock_MockServo.return_value.stop.assert_called_once_with()
+
+	"""
+	TESTING _buzz
+	"""
+	@mock.patch("_control_lock._setup", autospec=True)
+	def test_buzz_output(self, mock__setup):
+		mock__setup.return_value = GPIO.PWM(pins.SERVO_PIN, 50)
+		assert mock__setup is _control_lock._setup
+		_control_lock.main(commands.BUZZ)
+		calls = [mock.call(pins.BUZZER_PIN, GPIO.HIGH),
+				 mock.call(pins.BUZZER_PIN, GPIO.LOW)]
+		self.output_mock.assert_has_calls(calls)
 
 class MockServo:
 	def start(self, dummy=0):
@@ -107,5 +152,5 @@ class MockServo:
 
 
 if __name__ == '__main__':
-	suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
+	suite = unittest.TestLoader().loadTestsFromTestCase(TestServoControls)
 	unittest.TextTestRunner(verbosity=2).run(suite)
